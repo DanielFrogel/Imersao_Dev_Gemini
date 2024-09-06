c779d84769723959bd955beb16fb17c25816d935
DanielFrogel <daniel.frogel@gmail.com>
2024-09-06 13:40:31 -0300

    Aula 5

--- app.js
+++ app.js
@@ -1,24 +1,55 @@
+document.getElementById("campo-pesquisa").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+      // Função a ser executada quando a tecla Enter for pressionada
+      pesquisar();
+  }
+});
+
 function pesquisar() {
     // Seleciona a seção onde os resultados serão exibidos pelo ID
     let section = document.getElementById("resultados-pesquisa");
-  
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    let mensagem = document.getElementById("mensagem");
+
     // Inicializa uma string vazia para concatenar os resultados
     let resultados = "";
   
-    // Itera sobre cada dado no array 'dados'
+    if (campoPesquisa.trim() == ""){
+      mensagem.style.display = "block";
+      mensagem.className = "pesquisa-invalida";
+      mensagem.textContent = "*Campo pesquisa em Branco!";
+      return;
+    }   
+    
+    if (campoPesquisa.length < 3) {
+      mensagem.style.display = "block";
+      mensagem.className = "pesquisa-invalida";
+      mensagem.textContent = "*Pesquisa deve conter 3 ou mais caracteres!";      
+      return;      
+    }
+
+      // Itera sobre cada dado no array 'dados'
     for (let dado of dados) {
       // Constrói o HTML de cada resultado, formatando com template literals
       // O atributo 'target="_blank"' abre links em uma nova aba
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao} <a href="${dado.link}" target="_blank">Mais informações</a></p>
-        </div>
-      `;
+      if ((dado.titulo.toLowerCase().includes(campoPesquisa.toLowerCase())) || (dado.descricao.toLowerCase().includes(campoPesquisa.toLowerCase())) || (dado.tags.toLowerCase().includes(campoPesquisa.toLowerCase()))){
+        resultados += `
+          <div class="item-resultado">
+            <h2>
+              <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao} <a href="${dado.link}" target="_blank">Mais informações</a></p>
+          </div>
+        `;
+      }
+    }  
+
+    if (!resultados) {         
+      section.innerHTML = "<h2>A Pesquisa não retornou nenhum resultado!</h2>";  
     }
-  
-    // Atribui o HTML construído à seção de resultados
-    section.innerHTML = resultados;
-  }
+    else {
+      section.innerHTML = resultados;
+    }
+}
--- dados.js
+++ dados.js
@@ -2,17 +2,19 @@ let dados = [
     {
         titulo : 'Rayssa Leal',
         descricao : 'Rayssa Leal, conhecida como "Fadinha", é uma skatista brasileira que conquistou o mundo com seu talento precoce. Aos poucos anos de idade, ela já coleciona diversas medalhas em competições nacionais e internacionais, sendo um dos maiores nomes do skate feminino.',
-        link : 'https://pt.wikipedia.org/wiki/Rayssa_Leal'
+        link : 'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+        tags : 'skate, skatista, fadinha, esportes radicais, feminino, jovem, medalhista, olimpíadas, brasil, esporte brasileiro, ícone, inspiração, street, bowl, manobras, vert, olimpíada de tokyo'
     },
     {
         titulo : 'Rebeca Andrade',
         descricao : 'Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com sua habilidade e determinação. Com diversas medalhas olímpicas, incluindo ouro e prata, ela é considerada a maior ginasta da história do Brasil. Sua trajetória inspiradora e seu carisma a tornaram um símbolo do esporte nacional.',
-        link : 'https://pt.wikipedia.org/wiki/Rebeca_Andrade'
+        link : 'https://pt.wikipedia.org/wiki/Rebeca_Andrade',
+        tags : 'ginástica artística, ginasta, olimpíadas, brasil, esporte brasileiro, medalhista, ouro, prata, força, determinação, inspiração, salto, barras assimétricas, trave de equilíbrio, individual geral, ginástica olímpica'
     },
     {
         titulo : 'Beatriz Souza',
         descricao : 'Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg. Sua técnica apurada e determinação a colocaram como uma das principais atletas do judô brasileiro.',
-        link : 'https://pt.wikipedia.org/wiki/Beatriz_Souza'
-    }    
-    
+        link : 'https://pt.wikipedia.org/wiki/Beatriz_Souza',
+        tags : 'judô, judoca, olimpíadas, brasil, esporte brasileiro, medalhista, ouro, +78kg, luta, força, ippon, wazari, newaza, judô olímpico, judoca brasileira'
+    } 
 ]
